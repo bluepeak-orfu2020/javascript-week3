74981ce3540440f89a772580ff06a4eede9f6ded
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-02-02 11:29:09 +0100

    Added NPM

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,11 @@
 console.log('Before require');
+
 const math = require('./math.js');
+const devJokes = require('awesome-dev-jokes');
+
 console.log('After require');
 
 console.log('Square is: ' + math.square(3));
 console.log('Var1: ' + math.var1);
+
+console.log('Joke: ' + devJokes.getRandomJoke());
