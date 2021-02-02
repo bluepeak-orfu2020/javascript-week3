009a0baaac983bf65abccb7f5dcc090cb470318e
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-02-02 09:54:20 +0100

    Solution with IIFE

--- scope/color.js
+++ scope/color.js
@@ -1,16 +1,21 @@
+const color = (function() {
+    const colorElement = document.querySelector('.value')
 
-const colorElement = document.querySelector('.value')
-
-function value(num) {
-    if (num >= 12) {
-        return 'red';
-    } else {
-        return 'black';
+    function value(num) {
+        if (num >= 12) {
+            return 'red';
+        } else {
+            return 'black';
+        }
     }
-}
 
-function colorize() {
-    const val = colorElement.textContent;
-    const num = parseInt(val);
-    colorElement.style.color = value(num);
-}
+    function colorize() {
+        const val = colorElement.textContent;
+        const num = parseInt(val);
+        colorElement.style.color = value(num);
+    }
+
+    return {
+        colorize: colorize
+    }
+})()
--- scope/math.js
+++ scope/math.js
@@ -1,9 +1,15 @@
 
-let value = 10;
-const elem = document.querySelector('.value');
-elem.textContent = value;
-
-function increase() {
-    value++;
+const math = (function () {
+    let value = 10;
+    const elem = document.querySelector('.value');
     elem.textContent = value;
-}
+
+    function increase() {
+        value++;
+        elem.textContent = value;
+    }
+
+    return {
+        increase: increase
+    }
+})()
